f0342c9fcc31d0fa46652cd129379d3908f0be4f
test <2933134283@qq.com>
2016-05-10 16:46:33 +0800

    操作系统实验1
    经过测试与修改。

--- highestPriorityPrioritySchedulingAlgorithm/highestPriorityPrioritySchedulingAlgorithm/main.cpp
+++ highestPriorityPrioritySchedulingAlgorithm/highestPriorityPrioritySchedulingAlgorithm/main.cpp
@@ -1,10 +1,10 @@
 #include "course.h"
 
-void sort(course *c[],int k) {
+void sort(course c[],int k) {
 	for (int i = 0; i < k; i++) {
 		for (int j = i; j < k; j++) {
-			if (c[i]->getPrior() < c[j]->getPrior()) {
-				course *s = c[j];
+			if (c[i].getPrior() < c[j].getPrior()) {
+				course s = c[j];
 				c[j] = c[i];
 				c[i] = s;
 			}
@@ -13,26 +13,29 @@ void sort(course *c[],int k) {
 }
 
 void main() {
-	course *c[5] = { new course };
+	course c[5];
 	printf("请输入五组程序优先数及运行时间。\n");
 	for (int i = 0; i < 5; i++) {
 		int j;
-		printf("第%d组：\n优先数:", i);
-		scanf("%d", j);
-		c[i]->setPrior(j);
+		//c[i] = new course;
+		printf("第%d组：\n优先数:", i+1);
+		scanf_s("%d", &j);
+		c[i].setPrior(j);
 		printf("\n运行时间：");
-		scanf("%d", j);
-		c[i]->setNeedTime(j);
-		c[i]->setId(i);
+		scanf_s("%d", &j);
+		c[i].setNeedTime(j);
+		c[i].setId(i);
 		printf("\n");
 	}
 	sort(c, 5);
 	printf("程序运行的顺序为：\n");
+	int j = 0;
 	for (int i = 0; i < 5; i++) {
-		int j = 0;
-		printf("第%d个运行的程序ID为：%d；开始运行时间为：%d；", i, c[i]->getId(), j);
-		j += c[i]->getNeedTime;
+		printf("第%d个运行的程序ID为：%d；开始运行时间为：%d；", i + 1, c[i].getId() + 1, j);
+		j = j + c[i].getNeedTime();
 		printf("结束运行时间为：%d", j);
 		printf("\n");
 	}
+	getchar();
+	getchar();
 }
